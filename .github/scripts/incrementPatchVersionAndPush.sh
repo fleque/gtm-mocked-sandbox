a753b81d26685c4b8e73a2492dd2528eedcb6a46
Bieber, Alexander <alexander.bieber@haufe-lexware.com>
2024-03-15 22:01:24 +0100

    Trying to incrementPatchVersionAndPush.sh from bash script

---
+++ .github/scripts/incrementPatchVersionAndPush.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Running for commit: $GITHUB_SHA"
+IS_MERGE=$(git show --no-patch --format=%P $GITHUB_SHA | grep -c " ")
+echo "IS_MERGE: $IS_MERGE"
+if [ $IS_MERGE -eq 0 ]; then echo "Not a merge commit, abort step"; exit 0; fi
+NEW_VERSION=`npm version patch --no-git-tag-version`
+echo "Updating package.json with $NEW_VERSION"
+sed -i "s/\"version\": .*/\"version\": \"$NEW_VERSION\",/" package.json
+git config --local user.email "$GITHUB_ACTOR@users.noreply.github.com"
+git config --local user.name "$GITHUB_ACTOR"
+git add package.json
+git commit -m "Incremented patch version to $NEW_VERSION"
+echo "Pushing..."
+git push
